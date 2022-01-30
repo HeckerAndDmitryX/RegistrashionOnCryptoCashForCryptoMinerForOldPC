167c12f7dcebbe29960ad6aea2071e9b5572718c
MarkFireFox <76536605+MarkFireFox@users.noreply.github.com>
2022-01-30 14:38:36 +0000

    Add files via upload

---
+++ CryptoCashRegistration.py
@@ -0,0 +1,8 @@
+print("Registration on HeckerAmdDmitryX team cash")
+input("Enter you name:")
+input("Enter you last name:")
+input("Enter you age:")
+import random
+import string
+a=random = ''.join([random.choice(string.ascii_letters + string.digits ) for n in range(12)])
+print("You cash is",a,"DON'T FORGOT IT")
